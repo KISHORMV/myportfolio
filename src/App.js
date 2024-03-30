d74d7e3e4df770b0c4ad14b09d3062aafa4f63b2
KISHORMV <kishormuthu2@gmail.com>
2024-03-30 10:53:18 +0530

    removed footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
     <Projects/>
     <Resume/>
     <Contact/>
-    <Footer/>
+    
     </div>
   );
 }
